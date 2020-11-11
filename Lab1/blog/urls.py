592ddfbd3c6f39ba7c8f423e969755f78b67dd69
Jagoda Lewandowska <jadlewandowska@gmail.com>
2020-11-11 23:11:07 +0100

    Heroku

--- Lab1/blog/urls.py
+++ Lab1/blog/urls.py
@@ -5,8 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    #post/ url zaczynac sie bedzie od slowa post, po nim /
-    #<int:pk> django spodziewa się liczby całkowitej i przekaze jej wartosc do widoku jako zmienną pk
     path('post/<int:pk>', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
