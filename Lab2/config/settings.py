51508cc27e5401eaf4726ef519fc686770ca719c
Jagoda Lewandowska <jadlewandowska@gmail.com>
2020-11-17 11:11:32 +0100

    Fix gitignore (:

--- Lab2/config/settings.py
+++ Lab2/config/settings.py
@@ -12,16 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '111*-kh@(mrgg@)f39k!ka5t7t9&9u7$vu#3)2&naq3qjcmbjv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
