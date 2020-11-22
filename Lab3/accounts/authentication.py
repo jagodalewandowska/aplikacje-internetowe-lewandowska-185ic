744517700b121ea1a53ee3cfc7ac8f2ea5685da0
Jagoda Lewandowska <jadlewandowska@gmail.com>
2020-11-22 15:51:51 +0100

    heroku update

--- Lab3/accounts/authentication.py
+++ Lab3/accounts/authentication.py
@@ -6,6 +6,9 @@ class EmailAuthBackend(object):
     """
     Authenticate using an e-mail address.
     """
+    # Funkcja przyjmuje parametry jak username oraz hasło
+    # Próba pobrania użytkownika który sprawdzi hasło za pomocą funkcji check password
+    # porównując te z bazy danych
     def authenticate(self, request, username=None, password=None):
         try:
             user = User.objects.get(email=username)
@@ -15,6 +18,8 @@ class EmailAuthBackend(object):
         except User.DoesNotExist:
             return None
 
+    # pobranie użytkownika po jego ID, django używa backendu aby otrzymać zautentyfikowanego
+    # użytkownika przez cały czas trwania sesji
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
